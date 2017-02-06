35f2d3dfd53efe96dc4d503a76cdd0709604df1a
Catapult creëert <techniek@catapult.nl>
2017-02-06 15:07:03 +0100

    Update updater.class.php
    typo fix

--- updater.class.php
+++ updater.class.php
@@ -14,7 +14,7 @@ class GitHubPluginUpdater {
 		add_filter("pre_set_site_transient_update_plugins", array($this, "setTransitent"));
 		add_filter( "plugins_api", array( $this, "setPluginInfo" ), 10, 3 );
 		add_filter( "upgrader_pre_install", array( $this, "preInstall" ), 10, 3 );
-        add_filter( "upgrader_post_install", array( $this, "postInstall" ), 10, 3 );
+        	add_filter( "upgrader_post_install", array( $this, "postInstall" ), 10, 3 );
 		
 		$this->pluginFile = $pluginFile;
 		$this->username = $gitHubUsername;
